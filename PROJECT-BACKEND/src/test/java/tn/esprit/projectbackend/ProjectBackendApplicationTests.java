9971282af9380f1c2a66ff1c4f1842ccea84bae3
radhwenessefi <essefi.radhwen@esprit.tn>
2024-02-28 14:00:07 +0100

    first commit

---
+++ PROJECT-BACKEND/src/test/java/tn/esprit/projectbackend/ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
